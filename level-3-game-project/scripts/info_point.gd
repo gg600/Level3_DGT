4490ae670ccd60fef64a04de5e8b078168762d8a
CharlesMeurisse <charles.meurisse5@gmail.com>
2026-07-28 09:35:45 +1200

    InteractinUI
    Added an interaction UI box when clicking E next to an information point. Still need to add custom text to each of the information point UI.

--- level-3-game-project/scripts/info_point.gd
+++ level-3-game-project/scripts/info_point.gd
@@ -1,19 +1,31 @@
+
 extends Node3D
 
+@export var title := "Science Block"
 
-# Called when the node enters the scene tree for the first time.
+@export_multiline
+var information := "Information goes here."
+
+var player_near := false
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$Label3D.visible = false
+	$AnimationPlayer.play("Idle")
 
 
 func _on_area_3d_body_entered(body):
-	pass # Replace with function body.
-
+	if body.is_in_group("Player"):
+		player_near = true
+		$Label3D.visible = true
+		body.current_interactable = self
 
 func _on_area_3d_body_exited(body):
-	pass # Replace with function body.
+	if body.is_in_group("Player"):
+		player_near = false
+		$Label3D.visible = false
+
+		if body.current_interactable == self:
+			body.current_interactable = null
+
+func interact():
+	get_tree().current_scene.get_node("InteractionUI").show_information(title, information)
